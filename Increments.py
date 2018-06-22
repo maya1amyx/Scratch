48e115cd844b6d7f0b081847fa44b171fe14194a
Maya <maya.amyx@gmail.com>
2018-06-22 13:24:05 -0700

    Added Python Stuff
    IDK why I am adding Python stuff to a folder named Scratch, but it's what the teacher told me to do.

---
+++ Increments.py
@@ -0,0 +1,12 @@
+num1 = 0
+num2 = 0
+
+num1 = (input("Enter a number: "))
+num2 = (input("Enter a second number: "))
+#Adds an increment to num1
+num1 +=1
+
+num2 = num2 + 1 #reasigns the value of num2
+
+print(num1)
+print(num2)
---
+++ SimpleCalc.py
@@ -0,0 +1,17 @@
+#Initialize variables to 0 #Initializa is to asign a variable a value that can later be changed
+num1=0
+num2=0
+
+#input
+num1 = (input("Enter your first number: "))
+num2 = (input("Enter your second number: "))
+
+#Calculate sum
+sum = num1 + num2
+dif = num1 - num2
+product = num1 * num2
+
+#Output sum
+print("Here is your sum: %d " %sum)
+print("Here is your difference: %d" %dif)
+print("Here is your product: %d" %product)
